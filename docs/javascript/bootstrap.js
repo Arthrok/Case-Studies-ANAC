be191850bb9f925d611743be1ecba9eb2f576e91
arthrok <arthuralves1538@gmail.com>
2024-10-15 21:54:57 -0300

    movendo header option

--- docs/javascript/bootstrap.js
+++ docs/javascript/bootstrap.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const headerTitle = document.querySelector('.md-header__title');
     const headerInner = document.querySelector('.md-header__inner.md-grid');
     const search = document.querySelector('.md-header__button[for="__search"]');
+    const headerOption = document.querySelector('.md-header__option');
     
     // Apagar o elemento `.md-header__title`
     if (headerTitle) {
@@ -13,5 +14,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // Mover o elemento `.md-tabs` para dentro do `.md-header__inner.md-grid`, antes do botão de busca
     if (tabs && headerInner && search) {
         headerInner.insertBefore(tabs, search);
+    }
+
+    // Mover o elemento `headerOption` para dentro do `.md-header__inner.md-grid`, antes do botão de busca e depois das tabs
+    if (headerOption && headerInner && search) {
+        headerInner.insertBefore(headerOption, search);
     }
 });
